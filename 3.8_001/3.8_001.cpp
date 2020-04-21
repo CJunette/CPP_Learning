e3cc3da32af50b4da0914d27566e0eea08c5c79b
fengshuochange <fengshuochange@gmail.com>
2020-04-21 11:03:37 +0800

    Add project files.

---
+++ 3.8_001/3.8_001.cpp
@@ -0,0 +1,14 @@
+﻿// 3.8_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    //C++中的Lambda表达式
+    auto add = [](int a, int b) -> int {return a + b; };
+    
+    cout << add(1, 2) << endl;
+}
+
