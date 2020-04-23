a758df13800181c56568b3af67b6a62f959d3918
fengshuochange <fengshuochange@gmail.com>
2020-04-23 11:22:25 +0800

    Launch 3_homework_3-12.cpp

---
+++ 003/作业/3_homework_3-12/3_homework_3-12/3_homework_3-12.cpp
@@ -0,0 +1,29 @@
+﻿// 3_homework_3-12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int sumToZero(int n)
+{
+	if (n == 0)
+	{
+		cout << n;
+		return n;
+	}
+	else
+	{
+		cout << n << " + ";
+		return n + sumToZero(n - 1);
+	}
+}
+
+int main()
+{
+	cout << "Please input an integer number: ";
+	int n;
+	cin >> n;
+	int sum = sumToZero(n);
+	cout << " = " << sum << endl;
+}
+
