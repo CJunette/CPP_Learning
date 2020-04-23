39c7c54e926c7f97167f146cc6893554062cb709
fengshuochange <fengshuochange@gmail.com>
2020-04-23 15:14:05 +0800

    Launch 4.4_Constructor_001.cpp

--- 004/课程/4.4_Constructor_001/4.4_Constructor_001/4.4_Constructor_001.cpp
+++ 004/课程/4.4_Constructor_001/4.4_Constructor_001/4.4_Constructor_001.cpp
@@ -2,9 +2,57 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Clock
+{   
+    public:   
+    //定义构造函数
+    //构造函数要求与类名相同；不能定义返回值类型，也没有return语句；可以内联；可以重载；可以带默认参数。
+
+    //如果程序中已经存在了一个带参数的构造函数了，但我们还希望它有一个默认构造函数，则可以写成以下这种写法。    
+    Clock(int newH, int newM, int newS);
+    Clock() = default;
+    //默认的构造函数写法还有“Clock()”，如果类内没有定义构造器，编译器就会自动生成默认的构造函数。
+    //默认初始化会使类内的基本成员初始值不确定（垃圾数据），类内的其他类成员的初始值有对应类的初始化决定。
+
+
+    //要注意，如果上述的带参数的构造函数所有的形参都有默认值的话，他就自然地变成了一个默认构造函数。这时如果再在后面写“Clock()=default”，就会让编译器产生混淆，导致报错。
+    //即，如下写法是错误的。
+    /*
+    Clock(int newH = 0, int newM = 0, int newS = 0);
+    Clock() = default;
+    */
+
+    void setTime(int newH, int newM, int newS);
+    void showTime()
+    {
+        cout << hour << ":" << minute << ":" << second;
+    }
+
+    private:
+    int hour, minute, second;
+
+};
 
 int main()
 {
-    
+    Clock myClock;
+    int hour, minute, second;
+    cout << "Please input hour: ";
+    cin >> hour;
+    cout << "Please input minute: ";
+    cin >> minute;
+    cout << "Please input second: ";
+    cin >> second;
+    myClock.setTime(hour, minute, second);
+    myClock.showTime();
+
 }
 
+void Clock::setTime(int newH, int newM, int newS)
+{
+    hour = newH;
+    minute = newM;
+    second = newS;
+}
