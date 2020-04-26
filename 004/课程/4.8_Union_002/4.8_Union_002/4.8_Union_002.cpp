7098c4d4fa3f3ae4ab6770d23fef450d92a034ef
fengshuochange <fengshuochange@gmail.com>
2020-04-26 12:30:47 +0800

    Launch 4.8_Union_002.cpp

--- 004/课程/4.8_Union_002/4.8_Union_002/4.8_Union_002.cpp
+++ 004/课程/4.8_Union_002/4.8_Union_002/4.8_Union_002.cpp
@@ -1,4 +1,4 @@
-﻿// 4.8_Union_002.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// 4.8_Union_002.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //Example
 //union通常被植入class中使用。
 
@@ -9,13 +9,13 @@ using namespace std;
 class ExamInfo
 {
     public:
-    ExamInfo(string cours, char grd):
+    ExamInfo(string cours, char grd) :
         course(cours), mode(GRADE), grade(grd)
     {}
-    ExamInfo(string cours, bool pas):
+    ExamInfo(string cours, bool pas) :
         course(cours), mode(PASS), pass(pas)
     {}
-    ExamInfo(string cours, int pecnt):
+    ExamInfo(string cours, int pecnt) :
         course(cours), mode(PERCENT), percent(pecnt)
     {}
 
@@ -41,11 +41,11 @@ class ExamInfo
     private:
     string course;
 
-    //这里当我把enum的这行代码写成“enum mode {GRADE, PASS, PERCENT}”时，出现了报错（mode is not a nonstatic data member or base class of class）。
+    //这里如果我把enum的这行代码写成“enum mode {GRADE, PASS, PERCENT}”，会出现了报错（mode is not a nonstatic data member or base class of class）。
     //将鼠标移到代码错误时候的上面的mode上，会显示mode是一个enum类型的类。而实际上，在这里我们需要的是一个mode类的对象。
-    //这种“{}”在前，名称在后的写法。其实就相当于先写了“mode {GRADE, PASS, PERCENT}”，然后写“mode mode”创建了一个对象。（注意现实中不要让类名和变量名相同）
-    //而在修改之后我们再看代码就会发现,mode有了“field”的字样，说明它现在是一个对象成员。
-    enum {GRADE, PASS, PERCENT} mode;
+    //修改后的这种“{}”在前，名称在后的写法。其实就相当于先写了“mode {GRADE, PASS, PERCENT}”，然后写“mode mode”创建了一个对象。（注意现实中不要让类名和变量名相同）
+    //而在修改之后我们再将鼠标移到mode上会发现,mode有了“field”的字样，说明它现在是一个对象成员。
+    enum { GRADE, PASS, PERCENT } mode;
 
     union
     {
@@ -60,7 +60,7 @@ int main()
     ExamInfo course1("English", 'A');
     ExamInfo course2("Calculus", true);
     ExamInfo course3("C++ Programming", 95);
-    
+
     course1.showInfo();
     course2.showInfo();
     course3.showInfo();
