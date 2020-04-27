e4f068ff26f13f01bca26204d8aef00e36c92073
fengshuochange <fengshuochange@gmail.com>
2020-04-27 12:32:02 +0800

    Launch 4.10_Supplement_001.cpp

---
+++ 004/课程/4.10_Supplement_001/4.10_Supplement_001/4.10_Supplement_001.cpp
@@ -0,0 +1,45 @@
+﻿// 4.10_Supplement_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+double PI = 3.141592635898;
+
+class Circle
+{
+    public:
+    Circle(double rad) :
+        radius(rad)
+    {
+        perimeter = 2 * PI * radius;
+        area = PI * radius * radius;
+    }
+    Circle() :
+        Circle(0)
+    {}    
+    double getArea() { return area; }
+    double getPerimeter() { return perimeter; }
+
+    private:
+    double radius;
+    double perimeter;
+    double area;
+};
+
+
+int main()
+{
+    double originRadius;
+    cout << "Please input the origin radius of the pool: ";
+    cin >> originRadius;
+    Circle pool(originRadius);
+    Circle newPool(originRadius + 3);
+
+    double fencePrice = newPool.getPerimeter() * 35;
+    double aislePrice = (newPool.getArea() - pool.getArea()) * 20;
+
+    cout << "The price for fence is " << fencePrice << ".\n";
+    cout << "The price for aisle is " << aislePrice << ".\n";
+}
+
---
+++ 004/课程/4.10_Supplement_002/4.10_Supplement_002/4.10_Supplement_002.cpp
@@ -0,0 +1,95 @@
+﻿// 4.10_Supplement_002.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class SavingAccount
+{
+    private:
+    double balance, accumulation, rate;
+    int id, lastDate;
+    void record(double amount, int date)
+    {
+        accumulation += accumulate(date);
+        //保留两位小数。
+        amount = (amount >= 0) ? floor(amount * 100 + 0.5) / 100 : ceil(amount * 100 - 0.5) / 100;
+        balance += amount;
+        lastDate = date;
+        cout << "Date: " << date << "\t, id: " << id << "\t, amount:" << amount << "\t, balance: " << balance << endl;
+    }
+    const double accumulate(int date)
+    {
+        return accumulation + balance * (date - lastDate);
+    }
+
+    public:
+    SavingAccount(int i, int date, double r);
+    double getRate() { return rate; }
+    double getBalance() { return balance; }
+    void settle(int date);
+    void deposit(int date, double amount);
+    void withdraw(int date, double amount);
+    void show();
+    
+
+
+};
+
+SavingAccount::SavingAccount(int date, int i, double r) :
+    id(i), lastDate(date), rate(r)
+{
+    balance = 0;
+    accumulation = 0;
+}
+
+void SavingAccount::settle(int date)
+{
+    double interest = accumulate(date) * rate / 365;    
+    record(interest, date);
+    accumulation = 0;
+}
+
+void SavingAccount::deposit(int date, double amount)
+{
+    record(amount, date);
+}
+
+void SavingAccount::withdraw(int date, double amount)
+{
+    if (amount > balance)
+    {
+        cout << "Warning! You don't have enough money!!\n";
+    }
+    else
+    {
+        //注意这里是负值！
+        record(-amount, date);
+    }
+}
+
+void SavingAccount::show()
+{
+    cout << "id: " << id << "\t, balance: " << balance << endl;
+}
+
+
+int main()
+{
+    SavingAccount sa0(1, 21325302, 0.015);
+    SavingAccount sa1(1, 58320212, 0.015);
+
+    sa0.deposit(5, 5000);
+    sa1.deposit(25, 10000);
+    sa0.deposit(45, 5500);
+    sa1.withdraw(60, 4000);
+
+    sa0.settle(90);
+    sa1.settle(90);
+
+    sa0.show();
+    sa1.show();
+
+}
+
