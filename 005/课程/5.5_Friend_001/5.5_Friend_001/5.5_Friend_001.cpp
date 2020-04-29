4caa959864fe405b97161c23ab0d924d1144dfee
fengshuochange <fengshuochange@gmail.com>
2020-04-29 14:11:16 +0800

    Launch 5.5_Friend_001.cpp

--- 005/课程/5.5_Friend_001/5.5_Friend_001/5.5_Friend_001.cpp
+++ 005/课程/5.5_Friend_001/5.5_Friend_001/5.5_Friend_001.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //友元是一种在效率与安全性的权衡间为了提升效率而做出的设计。
 //因为它破坏了类的封装，因此要慎重地使用。
-//另外要注意！友元关系是单向的！且友元关系不能被继承。
+//另外要注意3点！1.友元关系不能传递！2.友元关系是单向的！3，友元关系不能被继承！
 
 //4.注意，这里必须要用一下前向引用声明。不然在Cal类中无法在成员函数形参中使用Point。
 class Point;
@@ -40,15 +40,18 @@ class Point
     int getX() { return x; }
     int getY() { return y; }
 
-    //1.友元函数是针对在本类之外的函数。这个函数不能是其他非友元类中的函数成员。
+    //1.友元函数是针对在本类之外的函数。
+    //1.这个函数非常特殊，它在类内声明，既可以在类内定义也可以在类外定义（都不会报错）；但不论是用本类对象调用，还是用静态函数调用方法调用，都无法调用友元函数。只有全局函数的调用方式能调用友元函数。
+    //1.这里就引出一个问题，友元函数为什么声明在类内，可是却不是类的函数成员。
+    //1.按我现在在网上找到的说法，基本都表示要在类外定义友元函数。也有说friend关键词所修饰的声明只是一个权限的声明，并不是真正意义上的声明，因此必须在类外在进行一次定义。（可能是C++版本更替对这点进行了修改和调整）
     //2.这里使用引用作为形参，进一步提高效率。但引用带来了双向传递的风险，老师说之后会给出解决。（我猜可能的做法就是加个const修饰）
-    friend double dist(Point &p1, Point &p2);    
+    friend double dist(Point &p1, Point &p2);
     //3.友元函数同样可以访问静态成员。
     friend void showCount();
 
-    //4.可以看到，即使我这里将Cal::dist设为友元函数，下面的函数定义中仍然无法访问到Point类的x与y对象。
+    //4.友元函数同样可以是其他类中的函数成员。
     friend double Cal::dist(Point &p1, Point &p2);
-    //5.必须要通过设置友元类，才能是其他类中的成员能访问这个类的私有成员。
+    //5.设置友元类可以使友元类中的所有成员都可以访问授权类中的private成员。
     //6.虽然这里把PointFriend类写在Point类中，但是实际上并不能通过Point的对象访问PointFriend类的成员。
     friend class PointFriend;
 
@@ -73,9 +76,9 @@ void showCount()
 
 double Cal::dist(Point &p1, Point &p2)
 {
-    //4.这种类似友元函数的写法并不能真正实现访问。
-    double x = (double)(p1.getX() - p2.getX());
-    double y = (double)(p1.getY() - p2.getY());
+    //4.以其他类中的函数成员作为友元函数同样可以实现对授权类对象private成员的访问。
+    double x = (double)(p1.x - p2.x);
+    double y = (double)(p1.y - p2.y);
     return sqrt(x * x + y * y);
 }
 
@@ -88,6 +91,9 @@ class PointFriend
         double y = (double)(p1.y - p2.y);
         return sqrt(x * x + y * y);
     }
+    private:
+    //5.友元类中的private成员同样可以访问授权类中的private成员。
+    int i = Point::count;
 };
 
 
@@ -97,16 +103,17 @@ int main()
     Point b(5, 1);
     cout <<"Dist: " << dist(a, b) << endl;
     showCount();
+    cout << endl;    
+    
+    cout << "Dist: " << Cal().dist(a, b) << endl;
     cout << endl;
 
     cout << "Dist: " << PointFriend().dist(a, b) << endl;
+    cout << endl;
 
     //7.析构函数可以主动调用。
     a.~Point();
     showCount();
 
-    PointFriend pf;
-    
-    
+    PointFriend pf;    
 }
-
