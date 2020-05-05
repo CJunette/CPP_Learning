be038071004c009779d0796cf2f5f70bdb303cdc
fengshuochange <fengshuochange@gmail.com>
2020-05-05 15:30:45 +0800

    Launch 5.8_Supplement_007_ProtectionOfSharedData

---
+++ 005/课程/5.8_Supplement_007_ProtectionOfSharedData/5.8_Supplement_007_ProtectionOfSharedData/5.8_Supplement_007_ProtectionOfSharedData.cpp
@@ -0,0 +1,30 @@
+﻿// 5.8_Supplement_007_ProtectionOfSharedData.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+    public:
+    void change() const
+    {
+        //常函数是可以改变静态成员的值的。
+        i++;
+    }
+
+    static int i;
+    int j = 0;
+};
+
+int A::i = 0;
+
+int main()
+{
+    A a;
+    cout << a.i << endl;
+    cout << a.j << endl;
+    a.change();
+    cout << a.i << endl;
+    cout << a.j << endl;
+}
