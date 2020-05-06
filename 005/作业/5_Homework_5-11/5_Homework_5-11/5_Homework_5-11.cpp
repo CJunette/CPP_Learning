628d5e683ac542fbaabc154b5ae2ba443a22beaf
fengshuochange <fengshuochange@gmail.com>
2020-05-06 17:07:04 +0800

    Launch 5_Homework_5-11

---
+++ 005/作业/5_Homework_5-11/5_Homework_5-11/5_Homework_5-11.cpp
@@ -0,0 +1,16 @@
+﻿// 5_Homework_5-11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int n;
+void fn1();
+
+int main()
+{
+    n = 0;
+    cout << n << endl;
+    fn1();
+    cout << n << endl;
+}
---
+++ 005/作业/5_Homework_5-11/5_Homework_5-11/fn.cpp
@@ -0,0 +1,8 @@
+
+extern int n;
+
+
+void fn1()
+{
+	n = 1;
+}
