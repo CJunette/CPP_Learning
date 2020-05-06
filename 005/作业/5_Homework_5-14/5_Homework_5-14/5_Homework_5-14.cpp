d8ff55be6985047c1a6efc48f6d3653649c0f8b4
fengshuochange <fengshuochange@gmail.com>
2020-05-06 17:11:11 +0800

    Launch 5_Homework_5-14

---
+++ 005/作业/5_Homework_5-14/5_Homework_5-14/5_Homework_5-14.cpp
@@ -0,0 +1,44 @@
+﻿// 5_Homework_5-14.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Car;
+
+class Boat
+{
+    public:
+    Boat(double w)
+    {
+        weight = w;
+    }
+
+    private:
+    double weight;
+    friend double getTotalWeight(Boat b, Car c);
+};
+
+class Car
+{
+    public:
+    Car(double w)
+    {
+        weight = w;
+    }
+
+    private:
+    double weight;
+    friend double getTotalWeight(Boat b, Car c);
+};
+
+double getTotalWeight(Boat b, Car c)
+{
+    return b.weight + c.weight;
+}
+
+int main()
+{
+    cout << getTotalWeight(Boat(3), Car(1.5)) << endl;
+}
+
