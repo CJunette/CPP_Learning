7a119c215bb281d38f0df57a13895c0ac9f60893
fengshuochange <fengshuochange@gmail.com>
2020-05-11 18:47:36 +0800

    Launch 6.6_Pointer_013_FunctionPointer

---
+++ 006/课程/6.6_Pointer_013_FunctionPointer/6.6_Pointer_013_FunctionPointer/6.6_Pointer_013_FunctionPointer.cpp
@@ -0,0 +1,45 @@
+﻿// 6.6_Pointer_013_FunctionPointer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//注意，函数指针（function pointer）是指指向的地址是函数的首地址的指针。
+//而之前提到的指针类型的函数（point function）是指返回值是指针的函数。
+//这里先给出书上的一个例子。我觉得这个例子比较好的反映了函数指针的定义、赋值相关的的内容。
+
+//另外，课上老师提到过，定义函数指针时需要“存储类型 数据类型 (*函数指针名)()”，这里的存储类型就是“static”这些。
+
+#include <iostream>
+using namespace std;
+
+void printStuff(float)
+{
+    cout << "Printing stuff..." << endl;
+}
+
+void printMessage(float data)
+{
+    cout << "Printing message: " << data << endl;
+}
+
+void printFloat(float data)
+{
+    cout << "Printing float: " << data << endl;
+}
+
+const float PI = 3.141592635898;
+
+int main()
+{
+    //在定义函数指针时，需要用“数据类型 (*函数指针名)(形参列表)”。其中函数指针名外的括号是十分重要的，因为去掉括号就变成了定义返回值为指针的函数。
+    //之所以有这些复杂的规则，说白了还是因为“指针指向对象的类型必须和指针自身的类型相符”这一原则。函数指针必须在定义时就确定好自己指向的目标的类型是怎样的。
+    void (*funcPtr)(float);
+
+    //注意这里有两个问题。
+    //1.这里的“&”可以加，也可以不加。因为单独的函数名也能代表函数所在的内存位置。
+    //2.这里有个C#里就遇到过的老问题。在设置函数指针的时候，只要具体函数即可，不需要加括号！
+    funcPtr = &printStuff;
+    //调用时，将实参传入函数指针的参数列表。
+    funcPtr(PI);
+
+    funcPtr = &printMessage;
+    funcPtr(PI);
+    funcPtr = &printFloat;
+    funcPtr(PI);
+}
