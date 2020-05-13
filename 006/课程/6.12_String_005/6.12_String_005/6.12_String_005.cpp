6af1e0531e27622b5872357439761d38b81aac28
fengshuochange <fengshuochange@gmail.com>
2020-05-13 18:13:43 +0800

    Launch 6.12_String_005

---
+++ 006/课程/6.12_String_005/6.12_String_005/6.12_String_005.cpp
@@ -0,0 +1,18 @@
+﻿// 6.12_String_005.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//getline()的一个具体例子。
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Please input: City Name, Country Name" << endl;
+        string city, country;
+        getline(cin, city, ',');
+        getline(cin, country);
+        cout << "City: " << city << "\tCountry: " << country << endl;
+    }
+}
