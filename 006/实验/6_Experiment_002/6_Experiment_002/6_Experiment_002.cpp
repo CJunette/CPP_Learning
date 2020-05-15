232d51bb21113e44279bacc389c814867f47535b
fengshuochange <fengshuochange@gmail.com>
2020-05-15 19:55:26 +0800

    Changes to 6_Experiment_002

--- 006/实验/6_Experiment_002/6_Experiment_002/6_Experiment_002.cpp
+++ 006/实验/6_Experiment_002/6_Experiment_002/6_Experiment_002.cpp
@@ -11,9 +11,13 @@ class Employee
         name(name), street(street), city(city), code(code)
     {}
     Employee():
-        Employee("null", "null", "null", 000000)
+        Employee("null", "null", "null", "000000")
     {}
-    void display()
+    ~Employee()
+    {
+        cout << "Destructor" << endl;
+    }
+    void display() const
     {
         cout << "Name: " << name << endl;
         cout << "Street: " << street << endl;
@@ -34,6 +38,7 @@ class Employee
 
 int main()
 {
+    //这部分不重要。
     Employee detective("John Watson", "221B_Baker_Street", "London", "221221");
     detective.display();
     detective.change_name("Sherlock Holmes");
@@ -44,10 +49,58 @@ int main()
         Employee("灰原哀", "帝丹小学1年A班", "米花市", "000002"),
         Employee("吉田步美", "帝丹小学1年A班", "米花市", "000003"),
         Employee("圆谷光彦", "帝丹小学1年A班", "米花市", "000004"),
-        Employee("小岛元太", "帝丹小学1年A班", "米花市", "000005"),
+        Employee("小岛元太", "帝丹小学1年A班", "米花市", "000005")
     };
-    for (Employee e: childDetectiveParty)
+    for (Employee const &e: childDetectiveParty)
     {
         e.display();
+    }    
+    
+    //另一种创建Employee数组的方式。
+    //此时用类的指针数组的方式对动态对象的地址进行储存，然后在最后用循环逐一对动态对象进行删除。
+    Employee *eply, *childDetectiveParty2[5];
+
+    eply = new Employee("江户川柯南", "帝丹小学1年A班", "米花市", "000001");
+    childDetectiveParty2[0] = eply;
+
+    eply = new Employee("灰原哀", "帝丹小学1年A班", "米花市", "000002");
+    childDetectiveParty2[1] = eply;
+
+    eply = new Employee("吉田步美", "帝丹小学1年A班", "米花市", "000003");
+    childDetectiveParty2[2] = eply;
+
+    eply = new Employee("圆谷光彦", "帝丹小学1年A班", "米花市", "000004");
+    childDetectiveParty2[3] = eply;
+
+    eply = new Employee("小岛元太", "帝丹小学1年A班", "米花市", "000005");
+    childDetectiveParty2[4] = eply;
+    //注意，此时进行删除的方式必须用循环遍历整个数组，然后逐一删除。
+    for (int i = 0; i < 5; i++)
+    {
+        delete childDetectiveParty2[i];
     }
+    
+    //另一种创建Employee数组的方式。
+    //此时用类的数组来储存动态对象经过复制构造函数创建的副本，然后在完成复制后立刻删除动态对象。
+    Employee *eply, childDetectiveParty3[5];
+
+    eply = new Employee("江户川柯南", "帝丹小学1年A班", "米花市", "000001");
+    childDetectiveParty3[0] = *eply;
+    delete eply;
+
+    eply = new Employee("灰原哀", "帝丹小学1年A班", "米花市", "000002");
+    childDetectiveParty3[1] = *eply;
+    delete eply;
+
+    eply = new Employee("吉田步美", "帝丹小学1年A班", "米花市", "000003");
+    childDetectiveParty3[2] = *eply;
+    delete eply;
+
+    eply = new Employee("圆谷光彦", "帝丹小学1年A班", "米花市", "000004");
+    childDetectiveParty3[3] = *eply;
+    delete eply;
+
+    eply = new Employee("小岛元太", "帝丹小学1年A班", "米花市", "000005");
+    childDetectiveParty3[4] = *eply;
+    delete eply;
 }
