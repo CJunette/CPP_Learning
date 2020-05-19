a42cffae701009f61d93695660dad03ffef51d39
fengshuochange <fengshuochange@gmail.com>
2020-05-19 19:16:15 +0800

    Launch 6_Homework_6-23

---
+++ 006/作业/6_Homework_6-23/6_Homework_6-23/6_Homework_6-23.cpp
@@ -0,0 +1,21 @@
+﻿// 6_Homework_6-23.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+const int N = 8;
+
+int main()
+{
+    cout << "Please input " << N << " scores: " << endl;
+	float sum = 0;
+	int j;
+	for (int i = 0; i < N; i++)
+	{		
+		cout << "Enter grade #" << i + 1 << ":";
+		cin >> j;
+		sum += j;
+	}
+	cout << "The average is " << sum / N << endl;
+}
