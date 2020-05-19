ee7ab5b8e140ccc19cf584a4654cfc9f2a9be084
fengshuochange <fengshuochange@gmail.com>
2020-05-19 19:15:47 +0800

    Launch 6_Homework_6-21

---
+++ 006/作业/6_Homework_6-21/6_Homework_6-21/6_Homework_6-21.cpp
@@ -0,0 +1,22 @@
+﻿// 6_Homework_6-21.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Please input a sentence:" << endl;
+    string s;
+    getline(cin, s);
+    int count = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+        {
+            count++;
+        }
+    }
+    cout << "There are " << count << " English letters." << endl;
+}
