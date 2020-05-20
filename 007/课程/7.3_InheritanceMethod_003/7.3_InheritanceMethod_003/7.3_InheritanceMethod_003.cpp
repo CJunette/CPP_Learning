106050b0608a058bbdbc51807d15fee376a90c13
fengshuochange <fengshuochange@gmail.com>
2020-05-20 18:38:41 +0800

    Launch 7.3_InheritanceMethod_003

---
+++ 007/课程/7.3_InheritanceMethod_003/7.3_InheritanceMethod_003/7.3_InheritanceMethod_003.cpp
@@ -0,0 +1,28 @@
+﻿// 7.3_InheritanceMethod_003.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//保护继承
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+    protected:
+    int a = 1;
+};
+class B: protected A
+{
+    public:
+    //对于类的成员而言，protected就相当于public，因此可以直接访问。
+    int getA() { return a; }
+};
+
+int main()
+{
+    A a;
+    //由于对于类外的对象而言，protected就相当于private，因此不能通过对象直接访问protected对象。
+    /*
+    a.x = 5;
+    */
+    B b;
+    cout << b.getA() << endl;
+}
