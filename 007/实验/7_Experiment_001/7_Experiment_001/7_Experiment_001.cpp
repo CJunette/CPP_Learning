92f6f842d0668dd59fc1ec27f1c7e50c06b263fb
fengshuochange <fengshuochange@gmail.com>
2020-05-27 18:19:34 +0800

    Launch 7_Experiment_001

---
+++ 007/实验/7_Experiment_001/7_Experiment_001/7_Experiment_001.cpp
@@ -0,0 +1,24 @@
+﻿// 7_Experiment_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+class Animal
+{
+    public:
+    int age;
+    private:
+};
+
+class Dog: public Animal
+{
+    public:
+    void setAge(int n) { age = n; }
+    private:
+};
+
+int main()
+{
+    Dog d;
+    d.setAge(2);
+}
