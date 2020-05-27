a87757b5d54d26283518d139988a0e7cd90416e3
fengshuochange <fengshuochange@gmail.com>
2020-05-27 18:25:44 +0800

    Launch 7_Experiment_003

---
+++ 007/实验/7_Experiment_003/7_Experiment_003/7_Experiment_003.cpp
@@ -0,0 +1,40 @@
+﻿// 7_Experiment_003.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+    public:
+    void run() { cout << "Vehicle run" << endl; }
+    void stop() { cout << "Vehicle stop" << endl; }
+    double maxSpeed, weight;    
+};
+
+class Bicycle: virtual public Vehicle
+{
+    public:
+    double height;    
+};
+
+class Motorcar: virtual public Vehicle
+{
+    public:
+    int seatNum;    
+};
+
+class Motorcycle: public Bicycle, public Motorcar
+{    
+};
+
+int main()
+{
+    Motorcycle m;
+    m.height = 1;
+    m.seatNum = 2;
+    m.weight = 100;
+    m.maxSpeed = 60;
+    m.run();
+    m.stop();
+}
