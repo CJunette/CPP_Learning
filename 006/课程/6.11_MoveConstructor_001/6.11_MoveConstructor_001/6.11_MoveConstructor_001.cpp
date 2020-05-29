58cc312b750b95acbd6a795f040b0f70826f161f
fengshuochange <fengshuochange@gmail.com>
2020-05-29 19:10:28 +0800

    Changes to 6.11_MoveConstructor_001

--- 006/课程/6.11_MoveConstructor_001/6.11_MoveConstructor_001/6.11_MoveConstructor_001.cpp
+++ 006/课程/6.11_MoveConstructor_001/6.11_MoveConstructor_001/6.11_MoveConstructor_001.cpp
@@ -85,8 +85,15 @@ A getA()
     */
 }
 
+IntNum &fun(IntNum i)
+{
+    return i;
+}
+
 int main()
 {    
+    fun(IntNum());
+    cout << endl;
     cout << getNum().getInt() << endl;
     cout << endl;
 
