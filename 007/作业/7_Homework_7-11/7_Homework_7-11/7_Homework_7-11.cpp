a128858efa53abbe8f6f8d448a5580a93d2b5865
fengshuochange <fengshuochange@gmail.com>
2020-05-29 19:11:10 +0800

    Launch 7_Homework_7-11

---
+++ 007/作业/7_Homework_7-11/7_Homework_7-11/7_Homework_7-11.cpp
@@ -0,0 +1,32 @@
+﻿// 7_Homework_7-11.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class BaseClass
+{
+    public:
+    void fn1() { cout << "fn1() in BaseClass" << endl; }
+    void fn2() { cout << "fn2() in BaseClass" << endl; }
+};
+
+class DerivedClass: public BaseClass
+{
+    public:
+    void fn1() { cout << "fn1() in DerivedClass" << endl; }
+    void fn2() { cout << "fn2() in DerivedClass" << endl; }
+};
+
+int main()
+{
+    DerivedClass dc;
+    DerivedClass *pdc = &dc;
+    BaseClass *pbc = &dc;
+
+    pdc->fn1();
+    pdc->fn2();
+    pbc->fn1();
+    pbc->fn2();
+}
+
