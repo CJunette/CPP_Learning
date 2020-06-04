6c7d6d2c624ef60b3379e9f640c4f5ba1fb47f3f
fengshuochange <fengshuochange@gmail.com>
2020-06-04 19:39:08 +0800

    Launch 8.3_VirtualFunction_008

---
+++ 008/课程/8.3_VirtualFunction_008/8.3_VirtualFunction_008/8.3_VirtualFunction_008.cpp
@@ -0,0 +1,28 @@
+﻿// 8.3_VirtualFunction_008.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//虚表与动态绑定。书上p340-343。
+
+#include <iostream>
+
+class Base
+{
+	public:
+	//虚表中包括f()与g()的地址。
+	virtual void f();
+	virtual void g();
+	private:
+	int i;
+};
+class Derived: public Base
+{
+	public:
+	//虚表中包括f()、g()与h()的地址。其中f()和h()指向派生类新定义的函数代码位置，g()指向基类中的函数代码位置。
+	virtual void f();
+	virtual void h();
+	private:
+	int j;
+};
+
+int main()
+{
+    
+}
