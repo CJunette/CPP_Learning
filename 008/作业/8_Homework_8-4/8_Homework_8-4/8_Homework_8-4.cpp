4d64908f483fefc2a549ef59b7640297c1ca985a
fengshuochange <fengshuochange@gmail.com>
2020-06-10 18:13:52 +0800

    Launch 8_Homework_8-4

---
+++ 008/作业/8_Homework_8-4/8_Homework_8-4/8_Homework_8-4.cpp
@@ -0,0 +1,24 @@
+﻿// 8_Homework_8-4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Counter
+{
+    public:
+    int i = 0;
+    Counter &operator +(int x) 
+    { 
+        i += x;
+        return *this;
+    }
+};
+
+int main()
+{
+    Counter ct;
+    cout << ct.i << endl;
+    ct = ct + 10;
+    cout << ct.i << endl;
+}
