4e965e4bfbca2a6d07988538e4ed4b4ed88e58f1
fengshuochange <fengshuochange@gmail.com>
2020-06-10 18:13:59 +0800

    Launch 8_Homework_8-5

---
+++ 008/作业/8_Homework_8-5/8_Homework_8-5/8_Homework_8-5.cpp
@@ -0,0 +1,23 @@
+﻿// 8_Homework_8-5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Mammal
+{
+    public:
+    virtual void speak() const { cout << "Mammal speaks" << endl; }
+};
+
+class Dog: public Mammal
+{
+    public:
+    virtual void speak() const { cout << "Dog speaks" << endl; }
+};
+
+int main()
+{
+    Dog d;
+    d.speak();
+}
