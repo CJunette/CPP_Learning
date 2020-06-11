99870d62c68897a9227904c48c5d53a64055d961
fengshuochange <fengshuochange@gmail.com>
2020-06-11 18:43:43 +0800

    Launch 8_HomeWork_8-9

---
+++ 008/作业/8_Homework_8-9/8_Homework_8-9/8_Homework_8-9.cpp
@@ -0,0 +1,39 @@
+﻿// 8_Homework_8-9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    public:
+    Base()
+    {
+        cout << "Base Constructor" << endl;
+    }
+    virtual ~Base() = 0;
+};
+Base::~Base()
+{
+    cout << "Base Destructor" << endl;
+}
+
+class Derived: public Base
+{
+    public:
+    Derived()
+    {
+        cout << "Derived Constructor" << endl;
+    }
+    virtual ~Derived()
+    {
+        cout << "Derived Destructor" << endl;
+    }
+};
+
+int main()
+{
+    Base *pb = new Derived;
+    cout << endl;
+    delete pb;
+}
