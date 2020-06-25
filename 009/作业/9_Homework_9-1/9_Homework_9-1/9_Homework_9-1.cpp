fbd0663451ef9a46a3a7bde4f9e2771b731fd69f
fengshuochange <fengshuochange@gmail.com>
2020-06-25 19:53:22 +0800

    Launch 9_Homework_9-1

---
+++ 009/作业/9_Homework_9-1/9_Homework_9-1/9_Homework_9-1.cpp
@@ -0,0 +1,23 @@
+﻿// 9_Homework_9-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Array.h"
+using namespace std;
+
+int main()
+{
+    cout << "Please input students number" << endl;
+    int n;
+    cin >> n;
+    cout << "Please input " << n << " score(s)" << endl;
+    Array<double> a(n);
+    double sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+    double average = sum / n;
+    cout << "The average score is " << average << endl;
+}
