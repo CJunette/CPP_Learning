87769e11807a0b50936066ee8ac6638bb5d1d026
fengshuochange <fengshuochange@gmail.com>
2020-07-02 20:48:46 +0800

    Launch 10.7_FunctionObject_003

---
+++ 010/课程/10.7_FunctionObject_003/10.7_FunctionObject_003/10.7_FunctionObject_003.cpp
@@ -0,0 +1,15 @@
+﻿// 10.7_FunctionObject_003.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//用STL库的标准对象multiplies来充当函数对象。
+
+#include <iostream>
+#include <numeric>
+#include <functional>
+using namespace std;
+
+int main()
+{
+    int a[] = { 1, 2, 3, 4, 5 };
+    int N = sizeof(a) / sizeof(int);
+    //multiplies是一个标准库中的、重载了“()”的类。
+    cout << accumulate(a, a + N, 1, multiplies<int>()) << endl;
+}
