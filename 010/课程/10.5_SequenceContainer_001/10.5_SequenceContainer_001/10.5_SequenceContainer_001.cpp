869214522a385d01d20b9fe19ef0d00b98109d91
fengshuochange <fengshuochange@gmail.com>
2020-07-03 20:06:56 +0800

    Changes to 10.5_SequenceContainer_001

--- 010/课程/10.5_SequenceContainer_001/10.5_SequenceContainer_001/10.5_SequenceContainer_001.cpp
+++ 010/课程/10.5_SequenceContainer_001/10.5_SequenceContainer_001/10.5_SequenceContainer_001.cpp
@@ -24,8 +24,28 @@ void printContainer(const char *msg, const T &s)
     //1.1.接下来，即使通过构造函数，重新将那个int类型的数据变为deque<int>（在这里无法实现，但在自己执行“=”操作时可以），还是会有问题。因为ostream_iterator的对“=”的重载中，是通过“<<”运算符来实现输出的，而“<<”本身并没有以deque<int>对象为参数的重载。
     //1.1.此时会希望在主函数中重载“<<”运算符以解决问题。但到此会有一个无法解决的问题，如果在成员函数中调用运算符，则此时主函数中的运算符重载将无法影响那个函数中的运算符。由于“<<”是在运算符“=”的重载成员函数中的，因此无论在主函数中如何进行重载，都无法对其产生影响。    
     //1.2.[回答] 最为正确且快捷的做法是，将下面的模板参数写为<T::value_type>，也就是说，去获得deque<T>中的那个模板参数T所对应的类型，来作为ostream_iterator<T>的模板参数。
+    
     copy(s.begin(), s.end(), ostream_iterator<T::value_type>(cout, " "));
+    
+    //1.3.[2020.7.3] 在进行过一次补充，这里的函数如果用一般类型的数组（如double[]）作为参数的话，就会报错。因为对于数组指针而言并没有value_type。
+    //1.3.较为通用的解决方法是，将参数写为iterator_traits<T>::value_type。由于iterator_traits<T>对T*进行过偏特化，所以能准确得到value_type。
+    //1.3.但这里这样的做法是行不通的，因为对于数组而言，不能使用.begin()或.end()这样的方式来获取迭代器。    
+    /*
+    copy(s.begin(), s.end(), ostream_iterator<typename iterator_traits<T::iterator>::value_type>(cout, " "));
+    */
     cout << endl;
+}
+
+//1.3.因此这里最合适的做法还是在专门为数组写一个模板的重载。
+//1.3.要注意这里的模板和函数的形式。因为一般情况下数组名是会被认为是指针的；如果不写成这个形式，会导致最后实例化并调用的还是上面的那个函数。
+template<class T, size_t N>
+void printContainer(const char *msg, const T(&a)[N])
+{
+    cout << msg << ": ";
+    for(int i = 0; i < N; i++)
+    {
+        cout << a[i] << " ";
+    }
 }
 
 int main()
@@ -59,4 +79,8 @@ int main()
     //4.s.assign()会在赋值之前，将容器内之前的元素都清除。
     s.assign(l.begin(), l.end());
     printContainer("deque at last", s);
+
+    cout << endl;
+    int a[] = { 1, 2, 3, 4, 5 };
+    printContainer("a", a);
 }
