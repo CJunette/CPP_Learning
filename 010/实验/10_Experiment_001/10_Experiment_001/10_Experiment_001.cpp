0c81b999058033e30ff278f73e69f7914e2f47fc
fengshuochange <fengshuochange@gmail.com>
2020-07-07 18:50:28 +0800

    Launch 10_Experiment_001

---
+++ 010/实验/10_Experiment_001/10_Experiment_001/10_Experiment_001.cpp
@@ -0,0 +1,20 @@
+﻿// 10_Experiment_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    deque<int> q;
+	for(int i = 0; i < 5; i++)
+	{
+		q.push_back(i);
+	}
+	while(!q.empty())
+	{
+		cout << q.front() << " ";
+		q.pop_front();
+	}
+}
