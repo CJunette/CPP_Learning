bb4b864db98991c38e09558ff52ca3a28f5d76ab
fengshuochange <fengshuochange@gmail.com>
2020-07-07 18:45:40 +0800

    Launch 10_Homework_10-1

---
+++ 010/作业/10_Homework_10-1/10_Homework_10-1/10_Homework_10-1.cpp
@@ -0,0 +1,17 @@
+﻿// 10_Homework_10-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	vector<int> s;
+	int length = 40;
+	for(int i = 0; i < length; i++)
+	{
+		s.push_back(i);
+		cout << "index: " << i << "\tcapacity: " << s.capacity() << endl;
+	}
+}
