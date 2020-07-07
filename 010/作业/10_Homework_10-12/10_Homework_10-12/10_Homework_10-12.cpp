0e9b1c814b94904ae033d42d0f6332099d5332b8
fengshuochange <fengshuochange@gmail.com>
2020-07-07 18:46:13 +0800

    Launch 10_Homework_10-12

---
+++ 010/作业/10_Homework_10-12/10_Homework_10-12/10_Homework_10-12.cpp
@@ -0,0 +1,24 @@
+﻿// 10_Homework_10-12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <ctime>
+using namespace std;
+
+int randNum()
+{
+    return rand() % 10;
+}
+
+int main()
+{
+    srand((int)time(0));
+    vector<int> v(10);    
+    generate(v.begin(), v.end(), randNum);
+    ostream_iterator<int> out(cout, " ");
+    copy(v.begin(), v.end(), out);
+    cout << endl;
+}
