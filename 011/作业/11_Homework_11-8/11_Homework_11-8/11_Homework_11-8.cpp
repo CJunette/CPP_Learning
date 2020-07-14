895986da427d27c12d93dec650c338f9489c279a
fengshuochange <fengshuochange@gmail.com>
2020-07-14 14:59:50 +0800

    Launch 11_Homework_11-8

---
+++ 011/作业/11_Homework_11-8/11_Homework_11-8/11_Homework_11-8.cpp
@@ -0,0 +1,16 @@
+﻿// 11_Homework_11-8.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << "Please input a number: ";
+    int a;
+    cin >> a;
+    cout << "Dec: " << a << endl;
+    cout << "Oct: " << resetiosflags(ios_base::dec) << setiosflags(ios_base::oct | ios_base::uppercase) << a << endl;
+    cout << "Hex: " << resetiosflags(ios_base::oct) << setiosflags(ios_base::hex | ios_base::uppercase) << a << endl;
+}
