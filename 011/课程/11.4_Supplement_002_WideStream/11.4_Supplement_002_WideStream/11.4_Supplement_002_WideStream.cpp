d90ce12435832eaac7464fb6ac9d6543c26d7d41
fengshuochange <fengshuochange@gmail.com>
2020-07-14 15:02:41 +0800

    Launch 11.4_Supplement_002_WideStream

---
+++ 011/课程/11.4_Supplement_002_WideStream/11.4_Supplement_002_WideStream/11.4_Supplement_002_WideStream.cpp
@@ -0,0 +1,28 @@
+﻿// 11.4_Supplement_002_WideStream.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    locale loc(".936");
+    wcout.imbue(loc);
+    wifstream in("article.txt");
+    in.imbue(loc);
+
+    wstring str;
+    //count用来统计行数。
+    int count = 1;
+    //getline在实现输入的同时也会移动文件位置指针。
+    while(getline(in, str))
+    {
+        if(str.find_first_of(L"人") != wstring::npos)
+        {
+            wcout << count << L":" << str << endl << endl;
+        }
+        count++;
+    }
+}
