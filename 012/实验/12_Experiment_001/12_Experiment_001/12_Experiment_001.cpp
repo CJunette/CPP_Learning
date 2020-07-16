19d99dbc8d245a90db9cf21accf2ebad942246b9
fengshuochange <fengshuochange@gmail.com>
2020-07-16 10:36:24 +0800

    Launch 12_Experiment_001

---
+++ 012/实验/12_Experiment_001/12_Experiment_001/12_Experiment_001.cpp
@@ -0,0 +1,31 @@
+﻿// 12_Experiment_001.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class CException
+{
+    public:
+    CException() {}
+	//我觉得这里把析构函数写成虚函数似乎没有什么意义。
+    virtual ~CException() {}
+	void Reason() const { cout << "CException" << endl; }
+};
+
+void fn1()
+{
+    throw CException();
+}
+
+int main()
+{
+	try
+	{
+		fn1();
+	}
+	catch(const CException &ce)
+	{
+		ce.Reason();
+	}
+}
